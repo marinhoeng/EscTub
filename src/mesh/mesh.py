b3e1860f2ee58cd0e23a6fa342108c968540e7b6
marinhoeng <marinhoengenharias@gmail.com>
2023-11-17 19:06:04 -0300

    Implement well section in the mesh EscTub-03

--- src/mesh/mesh.py
+++ src/mesh/mesh.py
@@ -1,170 +1,155 @@
 import numpy as np
-import TEC
+from typing import Sequence
+
+from attrs import define
+from thermal.tec import tec_section, calculate_tec_section
 
 
-class OperationalConditions:  # verificar se essa classe ficará aqui ou criaremos uma fb-operational_conditions
-
-    def __init__(self, Tenv_sc: float, Tenv_seabed: float, Tenv_res: float, Q_sc: float, P_res: float):
-        self.Tenv_sc: float = Tenv_sc
-        self.Tenv_seabed: float = Tenv_seabed
-        self.Tenv_res: float = Tenv_res
-        self.Q_sc: float = Q_sc
-        self.P_res: float = P_res
-
-
-class Geometry:
-
-    def __init__(self, dL_well: float, dL_flowline: float, dL_riser: float,
-                 L_well: float, L_flowline: float, L_riser: float,
-                 dh_well: float, dh_flowline: float, dh_riser: float,
-                 ε_well: float, ε_flowline: float, ε_riser: float,
-                 θ_well: float, θ_flowline: float, θ_riser: float):
-
-        self.dL_well: float = dL_well
-        self.dL_flowline: float = dL_flowline
-        self.dL_riser: float = dL_riser
-
-        self.L_well: float = L_well
-        self.L_flowline: float = L_flowline
-        self.L_riser: float = L_riser
-
-        self.sections_well: int = np.ceil(self.L_well / self.dL_well)
-        self.sections_flowline: int = np.ceil(L_flowline / self.dL_flowline)
-        self.sections_riser: int = np.ceil(L_riser / self.dL_riser)
-
-        self.L_total: float = self.L_well + self.L_flowline + self.L_riser
-
-        self.dh_well: float = dh_well
-        self.dh_flowline: float = dh_flowline
-        self.dh_riser: float = dh_riser
-
-        self.ε_well: float = ε_well
-        self.ε_flowline: float = ε_flowline
-        self.ε_riser: float = ε_riser
-
-        self.θ_well: float = θ_well
-        self.θ_flowline: float = θ_flowline
-        self.θ_riser: float = θ_riser
+@define(frozen=True)
+class InputData:
+    # Geometry
+    dL_well: float
+    dL_flowline: float
+    dL_riser: float
+    L_well: float
+    L_flowline: float
+    L_riser: float
+    dh_well: float
+    dh_flowline: float
+    dh_riser: float
+    ε_well: float
+    ε_flowline: float
+    ε_riser: float
+    θ_well: float
+    θ_flowline: float
+    θ_riser: float
+    # Thermal
+    Tenv_sc: float
+    Tenv_seabed: float
+    Tenv_res: float
+    tec_sections: Sequence[tec_section]
+    # Operational
+    Q_sc: float
+    P_res: float
+    T_res: float
+    IP: float
+    # PVT
+    API: float
+    dg: float
+    RGO: float
 
 
-# =====================================================================================================================
+@define(frozen=True)
+class OperationalData:
+    Q_sc: float
+    P_res: float
+    T_res: float
 
 
-class Mesh:
-    
-    # aq apenas coloco os argumentos que compõem a malha
-    # e que serão preenchidos pelo mesh_running no futuro
-
-    def __init__(self, dh: float, dL: float, θ: float, ε: float, T_env: float, MDi: float, TEC: float):
-        self.dh: float = dh
-        self.dL: float = dL
-        self.θ: float = θ
-        self.ε: float = ε
-        self.T_env: float = T_env
-        self.MDi: float = MDi
-        self.TEC: float = TEC
+@define(frozen=True)
+class GeometryData:
+    dh: float
+    εr: float
+    θ: float
+    tec: float
+    T_env: float
 
 
-def mesh_running(gm: Geometry, op: OperationalConditions, tec: TEC):
+@define(frozen=True)
+class FlowData:
+    Q: float
 
+
+@define(frozen=True)
+class MeshData:
+    geometry: GeometryData
+    dL: float
+    MDi: float
+    MDf: float
+
+
+def building_mesh(data: InputData) -> list[MeshData]:
     full_mesh_list = []
 
-    MDi_well = np.zeros(gm.sections_well)
-    dL_well = np.zeros(gm.sections_well)
+    # System data
+    sections_well = np.ceil(data.L_well / data.dL_well)
+    sections_flowline = np.ceil(data.L_flowline / data.dL_flowline)
+    sections_riser = np.ceil(data.L_riser / data.dL_riser)
 
-    MDi_flowline = np.zeros(gm.sections_flowline)
-    dL_flowline = np.zeros(gm.sections_flowline)
+    L_total = data.L_well + data.L_flowline + data.L_riser
 
-    MDi_riser = np.zeros(gm.sections_riser)
-    dL_riser = np.zeros(gm.sections_riser)
+    # Auxiliary functions
+    def calculate_env_temperature_gradient(
+        T_env_top: float, T_env_button: float, L_section: float
+    ) -> float:
+        return (T_env_top - T_env_button) / L_section
 
-    for i in range(1, gm.sections_well):  # trabalhando no poço
+    def calculate_env_temperature(MD: float, T_button: float, T_grad: float) -> float:
+        return T_button + T_grad * MD
 
-        dL_well[i] = gm.dL_well
+    def get_section_tec(MD: float, tec_sections: Sequence[tec_section]) -> float:
+        N_sections = len(tec_sections)
+        tec = np.nan
 
-        MDi_well[i] = MDi_well[i - 1] + gm.dL_well
+        for sec in range(N_sections):
+            mdi = tec_sections[sec].mdi
+            mdf = tec_sections[sec].mdf
 
-        if MDi_well[i] > gm.L_well:
+            if mdi <= MD < mdf:
+                tec = calculate_tec_section(tec_sections[sec])
+                break
+            else:
+                continue
 
-            dL_well[i] = gm.L_well - dL_well[i - 1]
+        return tec
 
-            MDi_well[i] = gm.L_well
+    # Well section data
+    dL = data.dL_well
+    L_well = data.L_well
+    dh_well = data.dh_well
+    θ_well = data.θ_well
+    ε_well = data.ε_well
+    T_grad_well = calculate_env_temperature_gradient(
+        T_env_top=data.Tenv_seabed, T_env_button=data.Tenv_res, L_section=data.L_well
+    )
+    T_well_button = InputData.Tenv_res
 
-        dh = gm.dh_well
-        θ = np.deg2rad(gm.θ_well)
-        ε = gm.ε_well
+    # Well Section
+    MDi = 0
+    MDf = 0
 
-        grad_T_well = (op.Tenv_seabed - op.Tenv_res) / gm.L_well
-        T_env = grad_T_well * MDi_well + op.Tenv_res
+    for i in range(0, sections_well):
+        MDi = MDf
 
-        if 2100 < MDi_well <= 3200:
+        MDf = MDi + dL
 
-            TEC = TEC.tec_p1
+        if MDf > L_well:
+            dL = L_well - MDi
+            MDf = L_well
 
-        elif 2000 < MDi_well <= 2100:
+        # Hydraulic diameter [m]
+        dh = dh_well
 
-            TEC = TEC.tec_p2
+        # Relative rugosity [-].
+        εr = ε_well / dh
 
-        elif 1300 < MDi_well <= 2000:
+        # Inclination [rad]
+        θ = np.deg2rad(θ_well)
 
-            TEC = TEC.tec_p3
+        tec = get_section_tec(MD=MDi, tec_sections=InputData.tec_sections)
 
-        elif 1000 < MDi_well <= 1300:
+        T_env = calculate_env_temperature(
+            MD=MDf, T_button=T_well_button, T_grad=T_grad_well
+        )
 
-            TEC = TEC.tec_p4
+        geometry = GeometryData(dh=dh, εr=εr, θ=θ, tec=tec, T_env=T_env)
 
-        elif 90 < MDi_well <= 1000:
+        mesh = MeshData(geometry=geometry, dL=dL, MDi=MDi, MDf=MDf)
 
-            TEC = TEC.tec_p5
-
-        else:
-
-            TEC = TEC.tec_p6
-
-    full_mesh_list.append(Mesh(dh=dh, dL=dL_well, θ=θ, ε=ε, T_env=T_env, MDi=MDi_well, TEC=TEC))
-
-    for i in range(1, gm.sections_flowline):  # trabalhando na flowline
-
-        dL_flowline[i] = gm.dL_flowline
-
-        MDi_flowline[i] = MDi_flowline[i - 1] + gm.dL_flowline
-
-        if MDi_flowline[i] > gm.L_flowline:
-
-            dL_flowline[i] = gm.L_flowline - dL_flowline[i - 1]
-
-            MDi_flowline[i] = gm.L_flowline
-
-        dh = gm.dh_flowline
-        θ = np.deg2rad(gm.θ_flowline)
-        ε = gm.ε_flowline
-        T_env = op.Tenv_seabed
-
-        TEC_flowline = TEC.tec_f
-
-    full_mesh_list.append(Mesh(dh=dh, dL=dL_flowline, θ=θ, ε=ε, T_env=T_env, MDi=MDi_flowline, TEC=TEC_flowline))
-
-    for i in range(1, gm.sections_riser):  # trabalhando no riser
-
-        dL_riser[i] = gm.dL_riser
-
-        MDi_riser[i] = MDi_riser[i - 1] + gm.dL_riser
-
-        if MDi_riser[i] > gm.L_riser:
-
-            dL_riser[i] = gm.L_riser - dL_riser[i - 1]
-
-            MDi_riser[i] = gm.L_riser
-
-        dh = gm.dh_riser
-        θ = np.deg2rad(gm.θ_riser)
-        ε = gm.ε_riser
-
-        grad_T_sea = (op.Tenv_sc - op.Tenv_seabed) / gm.L_riser
-        T_env = grad_T_sea * MDi_riser + op.Tenv_seabed
-
-        TEC_riser = TEC.tec_r
-
-    full_mesh_list.append(Mesh(dh=dh, dL=dL_riser, θ=θ, ε=ε, T_env=T_env, MDi=MDi_riser, TEC=TEC_riser))
+        full_mesh_list.append(mesh)
 
     return full_mesh_list
+
+
+def test_well() -> None:
+    assert 1 == 1
