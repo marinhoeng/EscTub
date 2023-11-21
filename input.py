36f001a7e4470c1a4ecf66eea6dc20a96980397c
flesru <103010405+flesru@users.noreply.github.com>
2023-11-21 20:04:41 -0300

    first commit input

---
+++ input.py
@@ -0,0 +1,21 @@
+# Dados de Entrada
+
+#Geometria
+L_well=5000 #m
+L_flowline= 5000 #m
+dh= 6#'' #mudar para dh flowline e dh riser
+
+
+#Operacional
+P_res=650 #bar
+T_res=
+IP= 60 #m³/d/bar
+
+#PVT
+API=30
+dg=0.72
+RGO=300 #sm³/sm³
+
+
+
+
