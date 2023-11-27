e788eb9a58e6e5e22ee8e8414c1fcc47c529bff3
caioagleal <caioagleal@gmail.com>
2023-11-27 16:18:18 -0300

    Correct Beggs and Brill

--- src/pressure/Beggs_Brill.py
+++ src/pressure/Beggs_Brill.py
@@ -1,6 +1,5 @@
 import math as m
 
-
 def calculate_Beggs_Brill(
     ρ_oil: float,
     λL: float,
@@ -11,113 +10,97 @@ def calculate_Beggs_Brill(
     θ: float,
     g: float
 ) -> float:
+
     F_rm = Vm ** 2 / (g * dh)
     L1 = 316 * λL ** 0.302
     L2 = 0.0009252 * λL ** (-2.4684)
     L3 = 0.10 * λL ** (-1.4516)
     L4 = 0.5 * λL ** (-6.738)
-
     Nlv = Vsl * (ρ_oil / (g * σog))**0.25
 
-    if (λL < 0.4 and F_rm >= L1) or (λL >= 0.4 and F_rm > L4):
-        Padrao = "Distribuido"
-        C = 0
-        ψ = 1
+    def calculate_HL_segregado(λL: float, Nlv: float, F_rm: float, θ: float) -> float:
+        a = 0.98
+        b = 0.4846
+        c = 0.0868
+
+        dlinha = 0.011
+        ee = -3.768
+        f = 3.539
+        gg = -1.614
+
+        C = (1 - λL) * m.log(dlinha * λL ** ee * Nlv ** f * F_rm ** gg)
+        ψ = 1 + C * (m.sin(1.8 * θ) - (0.333 * (m.sin(1.8 * θ)) ** 3))
+
+        HLo_segregado = (a * λL ** b) / F_rm ** c
+
+        HL_segregado = HLo_segregado * ψ
+
+        return HL_segregado
+
+    def calculate_HL_intermitente(λL: float, Nlv: float, F_rm: float, θ: float) -> float:
+        a = 0.845
+        b = 0.5352
+        c = 0.0173
+
+        dlinha = 2.960
+        ee = 0.305
+        f = -0.4473
+        gg = 0.0978
+
+        C = (1 - λL) * m.log(dlinha * λL ** ee * Nlv ** f * F_rm ** gg)
+        ψ = 1 + (C * (m.sin(1.8 * θ) - 0.333 * (m.sin(1.8 * θ)) ** 3))
+
+        HLo_intermitente = (a * λL ** b) / F_rm ** c
+        HL_intermitente = HLo_intermitente * ψ
+        return HL_intermitente
+
+    def calculate_HL_distribuido(λL: float, F_rm: float) -> float:
         a = 1.065
         b = 0.5824
         c = 0.0609
-        HLo = (a * λL ** b) / F_rm ** c
-        HL = HLo * ψ
 
-    elif (λL < 0.01 and F_rm < L1) or (λL >= 0.01 and F_rm < L2):
-        Padrao = "Segregado"
-        a = 0.98
-        b = 0.4846
-        c = 0.0868
-        dlinha = 0.011
-        ee = -3.768
-        f = 3.539
-        gg = -1.614
-        C = (1 - λL) * m.log(dlinha * λL ** ee * Nlv ** f * F_rm ** gg)
-        if C < 0:
-            C = 0
-            ψ = 1
-        else:
-            ψ = 1 + (C * m.sin(1.8 * θ) - 0.333 * (m.sin(1.8 * θ)) ** 3)
-        HLo = (a * λL ** b) / F_rm ** c
-        HL = HLo * ψ
+        ψ = 1
 
-    elif ((0.01 <= λL < 0.4) and (L3 <= F_rm <= L1)) or (λL >= 0.4 and (L3 <= F_rm <= L4)):
-        Padrao = "Intermitente"
-        a = 0.845
-        b = 0.5352
-        c = 0.0173
-        dlinha = 2.960
-        ee = 0.305
-        f = -0.4473
-        gg = 0.0978
-        C = (1 - λL) * m.log(dlinha * λL ** ee * Nlv ** f * F_rm ** gg)
-        if C < 0:
-            C = 0
-            ψ = 1
-        else:
-            ψ = 1 + (C * m.sin(1.8 * θ) - 0.333 * (m.sin(1.8 * θ)) ** 3)
-        HLo = (a * λL ** b) / F_rm ** c
-        HL = HLo * ψ
+        HLo_distribuido = (a * λL ** b) / F_rm ** c
 
-    elif λL >= 0.01 and (L2 <= F_rm <= L3):
-        Padrao = "Transição"
+        HL_distribuido = HLo_distribuido * ψ
+
+        return HL_distribuido
+
+    # segregado
+    # intermitente
+    # distribuido
+    # transicao (segregado e intermitente)
+
+    if (λL < 0.01 and F_rm < L1) or (λL >= 0.01 and F_rm < L2):  # segregado
+
+        HL = calculate_HL_segregado(λL=λL, Nlv=Nlv, F_rm=F_rm, θ=θ)
+
+    elif ((0.01 <= λL < 0.4) and (L3 <= F_rm <= L1)) or ((λL >= 0.4) and (L3 <= F_rm <= L4)):  # intermitente
+
+        HL = calculate_HL_intermitente(λL=λL, Nlv=Nlv, F_rm=F_rm, θ=θ)
+
+    elif (λL < 0.4 and F_rm >= L1) or (λL >= 0.4 and F_rm > L4):  # distribuido
+
+        HL = calculate_HL_distribuido(λL=λL, F_rm=F_rm)
+
+    elif λL >= 0.01 and (L2 <= F_rm <= L3):  # transicao
+
         A = (L3 - F_rm) / (L3 - L2)
 
-        # Hl - Segregado
-        a = 0.98
-        b = 0.4846
-        c = 0.0868
-        dlinha = 0.011
-        ee = -3.768
-        f = 3.539
-        gg = -1.614
-        C = (1 - λL) * m.log(dlinha * λL ** ee * Nlv ** f * F_rm ** gg)
-        if C < 0:
-            C = 0
-            ψ = 1
-        else:
-            ψ = 1 + (C * m.sin(1.8 * θ) - 0.333 * (m.sin(1.8 * θ)) ** 3)
-        HLo = (a * λL ** b) / F_rm ** c
-        HL_s = HLo * ψ
+        HL_s = calculate_HL_segregado(λL=λL, Nlv=Nlv, F_rm=F_rm, θ=θ)
+        HL_i = calculate_HL_intermitente(λL=λL, Nlv=Nlv, F_rm=F_rm, θ=θ)
 
-        # Hl - Intermitente
-        a = 0.845
-        b = 0.5352
-        c = 0.0173
-        dlinha = 2.960
-        ee = 0.305
-        f = -0.4473
-        gg = 0.0978
-        C = (1 - λL) * m.log(dlinha * λL ** ee * Nlv ** f * F_rm ** gg)
-        if C < 0:
-            C = 0
-            ψ = 1
-        else:
-            ψ = 1 + (C * m.sin(1.8 * θ) - 0.333 * (m.sin(1.8 * θ)) ** 3)
+        HL_transicao = A * HL_s + (1 - A) * HL_i
 
-        HLo = (a * λL ** b) / F_rm ** c
+        HL = HL_transicao
 
-        HL_i = HLo * ψ
-
-        # HL - Transição
-        HL = A * HL_s + (1 - A) * HL_i
     else:
         raise ValueError("BB failed.")
-
-    # if C < 0:
-    #     C = 0
-    #     ψ = 1
-    # else:
-    #     ψ = 1 + (C * m.sin(1.8 * θ) - 0.333 * (m.sin(1.8 * θ)) ** 3)
-    #
-    # HLo = (a * λL ** b) / F_rm ** c
-    #
-    # HL_i = HLo * ψ
-
     return HL
+
+
+HL = calculate_Beggs_Brill(51.207, 0.556, 17.915, 32.233, 1/3, 0.0005549, 90, 32.2) #aplicacao ex da aula - bateu o
+                                                                                    # resultado. uso de unidades
+                                                                                    #de campo
+print(HL)
